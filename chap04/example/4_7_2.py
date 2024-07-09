a83647e82071094018ee0af4a3d2e33bbfb5c23c
kingunwoo <keonu1111@gmail.com>
2024-07-09 16:47:02 +0900

    try1

--- chap04/example/4_7_2.py
+++ chap04/example/4_7_2.py
@@ -1,21 +1,21 @@
 import numpy as np
 import cv2
 
+# 전역 변수 선언
+image = np.ones((300, 300), np.uint8) * 255
+title = "Draw Event"
 
 def onMouse(event, x, y, flags, param):
-    global title
-    col = (255,0,0)
+    global image
+    col = (255, 0, 0)
     pt = (x, y)
-    if (event == cv2.EVENT_LBUTTONDOWN):
-        cv2.circle(image, pt, 100, col, 5,1)
-    elif(event == cv2.EVENT_RBUTTONDOWN):
-        cv2.rectangle(image, pt, pt+(30,30), col, 2)
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(image, pt, 100, col, 5, 1)
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        cv2.rectangle(image, pt, (pt[0] + 30, pt[1] + 30), col, 2)
     
     cv2.imshow(title, image)
-        
-image = np.ones((300,300), np.uint8) * 255
 
-title = "Draw Event"
 cv2.namedWindow(title)
 cv2.imshow(title, image)
 cv2.setMouseCallback(title, onMouse)
